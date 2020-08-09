304f1594f0dba033436ddecac4f062f6a4486863
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-09 03:54:14 -0700

    Added action type in users reducer to add user

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,5 +1,10 @@
 function usersReducer(state = [], action) {
-   return state;
+   switch (action.type) {
+      case  "ADD_USER":
+         return [...state, action.user]
+      default:
+         return state;
+   }
 }
 
 export default usersReducer;
