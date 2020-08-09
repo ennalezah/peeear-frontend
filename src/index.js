67e197fad7c78777dadd71697e84d16e862b4239
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-09 01:20:25 -0700

    Added import statements for createStore and Provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
