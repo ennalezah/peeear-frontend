0fb8608892190f798c4e12aceffdcfe9dba5064c
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-09 04:53:10 -0700

    Updated usersReducer to indicate loading users

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,7 +1,19 @@
-function usersReducer(state = [], action) {
+function usersReducer(state = { users: [], requesting: false }, action) {
    switch (action.type) {
-      case  "ADD_USER":
-         return [...state, action.user]
+      case 'GET_USERS_REQUEST':
+         return {
+            ...state,
+            users: [...state.users],
+            requesting: true 
+         }
+
+      case 'ADD_USER':
+         return {
+            ...state,
+            users: [...state.users],
+            requesting: false
+         }
+         
       default:
          return state;
    }
