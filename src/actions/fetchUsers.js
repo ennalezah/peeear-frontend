e601e5ac4cfd1790d29709db8702e7c65257f22f
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-09 17:21:27 -0700

    Updated title on index.html and usersUrl for fetch

--- src/actions/fetchUsers.js
+++ src/actions/fetchUsers.js
@@ -1,11 +1,15 @@
 export const fetchUsers = () => {
-   const usersUrl = 'http://localhost:3000/api/v1/users';
+   const usersUrl = 'http://localhost:3001/api/v1/users';
 
    return (dispatch) => {
       dispatch({ type: 'LOADING_USERS' });
 
       fetch(usersUrl)
          .then(resp => resp.json())
-         .then(users => dispatch({ type: 'ADD_USERS', users }));
+         .then(users => {
+             dispatch({ type: 'ADD_USERS', users })
+             console.log(users);
+            }
+         );
    };
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -12,7 +12,7 @@ import rootReducer from './reducers/rootReducer';
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Provider store={ store }>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
@@ -21,4 +21,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
