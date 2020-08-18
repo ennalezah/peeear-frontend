05edd81732ea72d1a9508591653e76c683ff561b
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-18 02:39:06 -0700

    Created NewProjectForm component

--- src/components/NewProjectContainer.js
+++ src/components/NewProjectContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import { NewProjectForm } from './NewProjectForm'
 
 class NewProjectContaner extends Component {
    constructor() {
@@ -36,34 +37,10 @@ class NewProjectContaner extends Component {
 
    render() {
       return(
-         <>
-         <h3>Add a New Project</h3>
-         <form onSubmit={this.handleSubmit}>
-            <p>
-               <input type="text" name="title" value={this.state.title} onChange={this.handleChange} placeholder="Title"/>
-            </p>
-
-            <p>
-               <textarea name="description" value={this.state.description} onChange={this.handleChange} placeholder="Describe your project in detail. What are some features on it? What can users do with it? What problem are you trying to solve?" />
-            </p>
-
-            <p>
-               <select name="difficulty" value={this.state.difficulty} onChange={this.handleChange} >
-                  <option hidden selected value>Difficulty (select one)</option>
-                  <option value="Beginner">Beginner</option>
-                  <option value="Intermediate">Intermediate</option>
-                  <option value="Advanced">Advanced</option>
-               </select>
-            </p>
-
-            <p>
-               <label>Owner ID</label>
-               <input type="text" name="owner_id" value={this.state.ownerId} onChange={this.handleChange} />
-            </p>
-
-            <input type="submit" value="Submit"/>
-         </form>
-         </>
+         <div>
+            <h1>Post a New Project</h1>
+            <NewProjectForm {...this.state} handleChange={ this.handleChange } handleSubmit={ this.handleSubmit }/>
+         </div>
       )
    }
 }
---
+++ src/components/NewProjectForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export const NewProjectForm = (props) => {
+   return(
+      <form onSubmit={props.handleSubmit}>
+         <p>
+            <input type="text" name="title" value={props.title} onChange={props.handleChange} placeholder="Title"/>
+         </p>
+
+         <p>
+            <textarea name="description" value={props.description} onChange={props.handleChange} placeholder="Describe your project in detail. What are some features on it? What can users do with it? What problem are you trying to solve?" />
+         </p>
+
+         <p>
+            <select name="difficulty" value={props.difficulty} onChange={props.handleChange} >
+               <option hidden selected value>Difficulty (select one)</option>
+               <option value="Beginner">Beginner</option>
+               <option value="Intermediate">Intermediate</option>
+               <option value="Advanced">Advanced</option>
+            </select>
+         </p>
+
+         <p>
+            <label>Owner ID</label>
+            <input type="text" name="owner_id" value={props.ownerId} onChange={props.handleChange} />
+         </p>
+
+         <input type="submit" value="Submit"/>
+      </form>
+   )
+}
