4a65c1e9c04b26a5782702193f4049dbe73f43a7
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-21 07:40:42 -0700

    Created Jumbotron component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { HowItWorks } from './HowItWorks';
+import { Jumbotron } from './Jumbotron';
 
 export const Home = () => {
    return(
       <>
-         <header className="App-header">
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
-            <h1>PEEEAR</h1> 
-            <h3>Improve your coding skills by pair programming on projects you're interested in</h3>   
-         </header>
+         <Jumbotron />         
          
          <HowItWorks />
       </>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,16 +3,19 @@ import  { Link } from 'react-router-dom';
 
 export const NavBar = () => {
    return(
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-         <a class="navbar-brand" href="/">PEEEAR</a>
-         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+         <a className="navbar-brand" href="/">
+            <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy" />
+            PEEEAR
+         </a>
+         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
          </button>
-         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-               <Link to="/" exact class="nav-link active">Home <span class="sr-only">(current)</span></Link>
-               <Link to='/projects' exact class="nav-link">Browse Projects</Link>
-               <Link to='/new-project' exact class="nav-link">Post New Project</Link>
+         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+               <Link to="/" exact className="nav-link active">Home <span className="sr-only">(current)</span></Link>
+               <Link to='/projects' exact className="nav-link">Browse Projects</Link>
+               <Link to='/new-project' exact className="nav-link">Post New Project</Link>
             </div>
          </div>
       </nav>
