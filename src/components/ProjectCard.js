7ca185118afa4c2b89e03952755761544070c76b
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-21 02:45:01 -0700

    Updated contact button link in ProjectCard

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -12,7 +12,7 @@ export const ProjectCard = (props) => {
          ~ ~ ~         
          
          <p><strong>Interested in pairing?</strong></p>
-         <button><a href="mailto: {props.email}">Contact {props.first_name}</a></button>
+   <button><a href={`mailto:${props.email}?subject=Re: Your PEEEAR project - ${props.title}&body=Hi ${props.first_name}! I'm interested in pairing up for this project. Please send more info my way. Thanks!"`}>Contact {props.first_name}</a></button>
          {/* <p>{props.first_name} {props.last_name}</p>
          <p>{props.email}</p> */}
 
