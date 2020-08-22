da6a03f5c1a678c6d3d04109e9ed23958badbf4d
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-22 00:26:58 -0700

    Styled NewProjectForm

--- src/components/NewProjectForm.js
+++ src/components/NewProjectForm.js
@@ -2,49 +2,80 @@ import React from 'react';
 
 export const NewProjectForm = (props) => {
    return(
-      <form onSubmit={props.handleSubmit}>
-         <p>
-            <select name="difficulty" value={props.difficulty} onChange={props.handleChange} required>
-               <option hidden selected value>Difficulty (select one)</option>
+      <div className="container">
+         <form onSubmit={props.handleSubmit}>
+            {/* <p>
+               <select name="difficulty" value={props.difficulty} onChange={props.handleChange} required>
+                  <option hidden selected value>Difficulty (select one)</option>
+                  <option value="Beginner">Beginner</option>
+                  <option value="Intermediate">Intermediate</option>
+                  <option value="Advanced">Advanced</option>
+               </select>
+            </p> */}
+
+            <div className="form-group">
+               <select className="form-control" name="difficulty" value={props.difficulty} onChange={props.handleChange} required>
+                  <option hidden selected value>Difficulty (select one)</option>
                <option value="Beginner">Beginner</option>
-               <option value="Intermediate">Intermediate</option>
-               <option value="Advanced">Advanced</option>
-            </select>
-         </p>
+                  <option value="Intermediate">Intermediate</option>
+                  <option value="Advanced">Advanced</option>
+               </select>
+            </div>
 
-         <p>
-            <input type="text" name="title" value={props.title} onChange={props.handleChange} placeholder="Title"/>
-         </p>
+            {/* <p>
+               <input type="text" name="title" value={props.title} onChange={props.handleChange} placeholder="Title"/>
+            </p> */}
 
-         <p>
-            <textarea name="description" value={props.description} onChange={props.handleChange} placeholder="Describe your project in detail. What are some features on it? What can users do with it? What problem are you trying to solve?" />
-         </p>
+            <div className="form-group">
+               <input type="text" className="form-control" name="title" value={props.title} onChange={props.handleChange} placeholder="Project title" />
+            </div>
 
-         {/* <p>
-            <select name="difficulty" value={props.difficulty} onChange={props.handleChange} >
-               <option hidden selected value>Difficulty (select one)</option>
-               <option value="Beginner">Beginner</option>
-               <option value="Intermediate">Intermediate</option>
-               <option value="Advanced">Advanced</option>
-            </select>
-         </p> */}
+            {/* <p>
+               <textarea name="description" value={props.description} onChange={props.handleChange} placeholder="Describe your project in detail. What are some features on it? What can users do with it? What problem are you trying to solve?" />
+            </p> */}
 
-         <h3>Contact info</h3>
-         <p>This is how individuals will reach out to you about your project. Please fill info out as accurately as possible.</p>
+            <div className="form-group">
+               <textarea className="form-control" name="description" rows="4" value={props.description} onChange={props.handleChange} placeholder="Describe your project in detail. What problem are you trying to solve with it? List some features you'd like to build out. What can users do with it? What language(s) are you using to create your project?"></textarea>
+            </div>
 
-         <p>
-            <input type="text" name="first_name" value={props.first_name} onChange={props.handleChange} placeholder="First name" required/>
-         </p>
+            {/* <p>
+               <select name="difficulty" value={props.difficulty} onChange={props.handleChange} >
+                  <option hidden selected value>Difficulty (select one)</option>
+                  <option value="Beginner">Beginner</option>
+                  <option value="Intermediate">Intermediate</option>
+                  <option value="Advanced">Advanced</option>
+               </select>
+            </p> */}
 
-         <p>
-            <input type="text" name="last_name" value={props.last_name} onChange={props.handleChange} placeholder="Last name" required/>
-         </p>
+            <h5>Contact info</h5>
+            <p>This is how individuals will reach out to you about your project. Please fill info out as accurately as possible.</p>
 
-         <p>
-            <input type="text" name="email" value={props.email} onChange={props.handleChange} placeholder="Email" required />
-         </p>
+            {/* <p>
+               <input type="text" name="first_name" value={props.first_name} onChange={props.handleChange} placeholder="First name" required/>
+            </p> */}
 
-         <input type="submit" value="Submit"/>
-      </form>
+            <div className="form-group">
+               <input type="text" className="form-control" name="first_name" value={props.first_name} onChange={props.handleChange} placeholder="First name" required />
+            </div>
+
+            {/* <p>
+               <input type="text" name="last_name" value={props.last_name} onChange={props.handleChange} placeholder="Last name" required/>
+            </p> */}
+
+            <div className="form-group">
+               <input type="text" className="form-control" name="last_name" value={props.last_name} onChange={props.handleChange} placeholder="Last name" required />
+            </div>
+
+            {/* <p>
+               <input type="text" name="email" value={props.email} onChange={props.handleChange} placeholder="Email" required />
+            </p> */}
+            
+            <div className="form-group">
+               <input type="email" className="form-control" name="email" value={props.email} onChange={props.handleChange} placeholder="Email" required />
+            </div>
+
+            <button type="submit" className="btn btn-primary">Submit</button>
+         </form>
+      </div>
    )
 }
