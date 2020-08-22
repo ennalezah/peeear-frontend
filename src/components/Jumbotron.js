e2c1d8c953ee5ab2398f8c4db383698e6837eadf
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-22 06:15:33 -0700

    Added styling for Jumbotron

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -4,7 +4,7 @@ export const Jumbotron = () => {
    return(
       <div className="jumbotron vertical-center">
          <div className="container">
-            <h1 className="display-4">PEEEAR</h1>
+            <h1 className="jumbotron-title">PEEEAR</h1>
             <p className="lead">Improve your coding skills by pair programming on projects you're interested in</p>   
             <br />      
             <a className="btn btn-primary btn-lg" href="/projects" role="button">Browse Projects</a>   
