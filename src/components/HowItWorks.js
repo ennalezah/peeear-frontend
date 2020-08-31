259a8bd440979f4a35b3e63a41e325b2fa00c1dc
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-30 23:40:14 -0700

    Changed icons width

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -6,22 +6,24 @@ import repeat from '../images/repeat.png';
 export const HowItWorks = () => {
    return(
       <div id="how-it-works">
-         <div className="container">
+         <div className="container py-4">
             <h6 className="text-uppercase">How It Works</h6>
             <br />
-            <div class="row align-items-center">
-               <div class="col">
-                  <img className="icons mb-4" src={project} alt="lightbulb"/>
+            <div className="row align-items-center">               
+               <div className="col">
+                  <img className="icons mb-4" src={project} alt="lightbulb" width="110" />
                   <h5>Find a project</h5>
                   <p>Browse all projects and find one that you like and will challenge you.</p>
                </div>
-               <div class="col">
-                  <img className="icons mb-4" src={teamwork} alt="hands"/>
+
+               <div className="col">
+                  <img className="icons mb-4" src={teamwork} alt="hands" width="110" />
                   <h5>Work as a Pair</h5>
                   <p>Contact the project's poster to let them know you're interested in pairing up.</p>
                </div>
-               <div class="col">
-                  <img className="icons mb-4" src={repeat} alt="repeat arrow"/>
+
+               <div className="col">
+                  <img className="icons mb-4" src={repeat} alt="repeat arrow" width="110" />
                   <h5>Repeat</h5>
                   <p>Complete the project with your partner, then return to step 1. </p>
                </div>
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -7,7 +7,7 @@ export const Jumbotron = () => {
             <h1 className="jumbotron-title">Two heads are better than one</h1>
             <p className="lead">Improve your coding skills by pair programming on projects you're interested in</p>   
             <br />      
-            <a className="btn btn-primary btn-lg" href="/projects" role="button">Browse Projects</a>   
+            <a className="btn btn-warning btn-lg" href="/projects" role="button">Browse Projects</a>   
          </div>         
       </div>
    )
