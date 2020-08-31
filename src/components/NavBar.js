c10ee6dd499472b8dfedb9313121012960b422b6
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-30 17:56:45 -0700

    Added logo to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import  { Link } from 'react-router-dom';
+import logo from '../images/logo.png';
 
 export const NavBar = () => {
    return(
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light">
          <a className="navbar-brand" href="/">
-            <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy" />
-            PEEEAR
+            <img src={logo} width="20" margin="10" className="d-inline-block align-top" alt="" loading="lazy" /> PEEEAR
          </a>
          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
