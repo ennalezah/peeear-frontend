3804fa78451803c8754c5db6fae58c6f83bf8872
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-08-30 22:53:41 -0700

    Added background color to HowItWorks

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -5,24 +5,26 @@ import repeat from '../images/repeat.png';
 
 export const HowItWorks = () => {
    return(
-      <div className="container">
-         <h6 className="text-uppercase">How It Works</h6>
-         <br />
-         <div class="row align-items-center">
-            <div class="col">
-               <img className="icons mb-4" src={project} alt="lightbulb"/>
-               <h5>Find a project</h5>
-               <p>Browse all projects and find one that you like and will challenge you.</p>
-            </div>
-            <div class="col">
-               <img className="icons mb-4" src={teamwork} alt="hands"/>
-               <h5>Work as a Pair</h5>
-               <p>Contact the project's poster to let them know you're interested in pairing up.</p>
-            </div>
-            <div class="col">
-               <img className="icons mb-4" src={repeat} alt="repeat arrow"/>
-               <h5>Repeat</h5>
-               <p>Complete the project with your partner then return to step 1 to keep improving. </p>
+      <div id="how-it-works">
+         <div className="container">
+            <h6 className="text-uppercase">How It Works</h6>
+            <br />
+            <div class="row align-items-center">
+               <div class="col">
+                  <img className="icons mb-4" src={project} alt="lightbulb"/>
+                  <h5>Find a project</h5>
+                  <p>Browse all projects and find one that you like and will challenge you.</p>
+               </div>
+               <div class="col">
+                  <img className="icons mb-4" src={teamwork} alt="hands"/>
+                  <h5>Work as a Pair</h5>
+                  <p>Contact the project's poster to let them know you're interested in pairing up.</p>
+               </div>
+               <div class="col">
+                  <img className="icons mb-4" src={repeat} alt="repeat arrow"/>
+                  <h5>Repeat</h5>
+                  <p>Complete the project with your partner, then return to step 1. </p>
+               </div>
             </div>
          </div>
       </div>
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -4,7 +4,7 @@ export const Jumbotron = () => {
    return(
       <div className="jumbotron vertical-center">
          <div className="container">
-            <h1 className="jumbotron-title">PEEEAR</h1>
+            <h1 className="jumbotron-title">Two heads are better than one</h1>
             <p className="lead">Improve your coding skills by pair programming on projects you're interested in</p>   
             <br />      
             <a className="btn btn-primary btn-lg" href="/projects" role="button">Browse Projects</a>   
